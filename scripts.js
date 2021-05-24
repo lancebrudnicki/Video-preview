769903f5176c3a43dddcd4caf7e82c208a7480ce
lancebrudnicki <lancebrudnicki@gmail.com>
2021-05-24 13:38:38 -0600

    Video

---
+++ scripts.js
@@ -0,0 +1,35 @@
+
+
+
+function signout(){
+    document.getElementById('signout')
+    alert('Dont Leave ME');
+}
+
+
+
+
+
+
+
+// function mute(){
+//     document.getElementById("myVideo").muted = true;
+// }
+
+function playvideo(){
+    document.getElementById("myVideo").play();
+    // console.log(el.parentNode.querySelector(video))
+}
+function pause(){
+    document.getElementById("myVideo").pause();
+}
+
+
+
+
+
+
+
+
+
+
